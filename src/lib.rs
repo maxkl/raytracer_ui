3ff46599eb915189f414422e45081ad3e8d4645d
maxkl <max@maxkl.de>
2019-05-16 19:59:51 +0200

    Move code into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+
+use image::{DynamicImage, GenericImage, GenericImageView, Rgb, Pixel};
+
+pub fn main() -> i32 {
+    let mut img = DynamicImage::new_rgb8(800, 600);
+
+    let w = img.width();
+    let h = img.height();
+    for y in 0..h {
+        for x in 0..w {
+            let r = (255 * x / w) as u8;
+            let g = (255 * y / h) as u8;
+            let b = 100;
+            let color = Rgb([r, g, b]);
+            img.put_pixel(x, y, color.to_rgba());
+        }
+    }
+
+    img.save("output.png").unwrap();
+
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 
-use image::{DynamicImage, GenericImage, GenericImageView, Rgb, Pixel};
+use std::process;
+
+use raytracer;
+
+fn wrapped_main() -> i32 {
+    raytracer::main()
+}
 
 fn main() {
-    let mut img = DynamicImage::new_rgb8(800, 600);
-
-    let w = img.width();
-    let h = img.height();
-    for y in 0..h {
-        for x in 0..w {
-            let r = (255 * x / w) as u8;
-            let g = (255 * y / h) as u8;
-            let b = 100;
-            let color = Rgb([r, g, b]);
-            img.put_pixel(x, y, color.to_rgba());
-        }
-    }
-
-    img.save("output.png").unwrap();
+    let exit_code = wrapped_main();
+    process::exit(exit_code);
 }
