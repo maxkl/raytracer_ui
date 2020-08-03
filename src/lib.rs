2130b2225b5fa6ff66a1b08cb0a580ed5a2f8f95
maxkl <max@maxkl.de>
2020-08-03 21:09:15 +0200

    Refactor to so that raytracer code is usable as a library

--- src/lib.rs
+++ src/lib.rs
@@ -8,34 +8,5 @@ mod lights;
 mod scene;
 mod renderer;
 
-use std::io::ErrorKind;
-use std::time::Instant;
-
-use crate::scene::Scene;
-
-/// Render the scene and store the resulting image at `output_file_name`
-pub fn main(scene_file_name: &str, output_file_name: &str) -> i32 {
-    // Load scene from scene definition file
-    let scene = Scene::load(scene_file_name).unwrap();
-
-    let now = Instant::now();
-
-    // Render scene
-    let img = renderer::render(&scene);
-
-    let duration = now.elapsed();
-    println!("Rendered scene in {:.3} ms", duration.as_micros() as f64 * 1e-3);
-
-    // Save image
-    if let Err(err) = img.save(output_file_name) {
-        match err.kind() {
-            ErrorKind::InvalidInput => {
-                eprintln!("Error: invalid file extension");
-                return 1;
-            }
-            _ => {}
-        }
-    }
-
-    0
-}
+pub use scene::Scene;
+pub use renderer::Renderer;
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,52 @@
 
-use std::{env, process};
+use std::{env, process, fs};
+use std::time::Instant;
 
-use raytracer;
+use raytracer::{Scene, Renderer};
+use std::error::Error;
 
-fn wrapped_main() -> i32 {
+/// Load a scene from a scene definition file in RON format
+pub fn load_scene(scene_file_name: &str) -> Result<Scene, Box<dyn Error>> {
+    // Load file as string
+    let source = fs::read_to_string(scene_file_name)?;
+
+    // Deserialize scene from string
+    let scene = ron::de::from_str(&source)?;
+
+    Ok(scene)
+}
+
+/// Render the scene and store the resulting image at `output_file_name`
+pub fn render_scene_file(scene_file_name: &str, output_file_name: &str) -> Result<(), Box<dyn Error>> {
+    let scene = load_scene(scene_file_name)?;
+
+    let now = Instant::now();
+
+    let renderer = Renderer::new(scene);
+    let img = renderer.render();
+
+    let duration = now.elapsed();
+    println!("Rendered scene in {:.3} ms", duration.as_micros() as f64 * 1e-3);
+
+    img.save(output_file_name)?;
+
+    Ok(())
+}
+
+fn main() {
     let args = env::args().collect::<Vec<_>>();
 
     if args.len() != 3 {
         eprintln!("Error: incorrect number of arguments");
         eprintln!("Usage: {} <scene file name> <output file name>", args[0]);
-        return 1;
+        process::exit(1);
     }
 
     let scene_file_name = &args[1];
     let output_file_name = &args[2];
 
-    // Call into lib.rs
-    raytracer::main(scene_file_name, output_file_name)
-}
-
-fn main() {
-    // Wrap main to simplify returning an exit code
-    let exit_code = wrapped_main();
-    process::exit(exit_code);
+    if let Err(err) = render_scene_file(scene_file_name, output_file_name) {
+        eprintln!("Error: {}", err);
+        process::exit(1);
+    }
 }
